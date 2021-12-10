06f4689093060c502b425eff9f0c0c5491dbb8b8
luanmoraes <luanmoraesb@gmail.com>
2021-12-09 22:32:01 -0400

    Adicionei um texto no arquivo

--- src/main/java/br/com/lojinha/LojinhaApp.java
+++ src/main/java/br/com/lojinha/LojinhaApp.java
@@ -8,6 +8,9 @@ import br.com.lojinha.pojo.ProdutoNacional;
 import java.util.ArrayList;
 import java.util.List;
 
+//adicionando um texto de alteração
+
+
 public class LojinhaApp {
     public static void main(String[] args) {
         Produto meuProduto = new Produto("PS4","Sony",Tamanho.MEDIO);  //instanciando um novo produto
